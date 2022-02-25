f8c443da8e579af7d8455913e67bf5c3ef24c96d
Slim Lim <slim@sarahlim.com>
2022-02-25 13:41:59 -0800

    Add jedi integration for Python

--- home.nix
+++ home.nix
@@ -389,6 +389,15 @@
           let g:ale_fix_on_save = 1
           let g:ale_completion_enabled = 1
 
+          let g:ale_linters = {
+          \ 'python': ['jedils', 'flake8'],
+          \ }
+
+          let g:ale_fixers = {
+          \ 'python': ['black'],
+          \ 'rust': ['rustfmt'],
+          \ }
+
           nmap <silent> [e   <Plug>(ale_previous_wrap)
           nmap <silent> ]e   <Plug>(ale_next_wrap)
 
