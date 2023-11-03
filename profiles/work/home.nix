aa2afc46fd82bfbefc525c0683d3ee9f73664c6c
Slim Lim <slim@makenotion.com>
2023-11-03 16:49:53 -0400

    VS Code

--- profiles/work/home.nix
+++ profiles/work/home.nix
@@ -34,4 +34,37 @@
   programs.firefox.extensions = with pkgs.nur.repos.rycee.firefox-addons; [
     onepassword-password-manager
   ];
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      github.copilot
+      esbenp.prettier-vscode
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "ayu";
+        publisher = "teabyii";
+        version = "1.0.5";
+        sha256 = "sha256-+IFqgWliKr+qjBLmQlzF44XNbN7Br5a119v9WAnZOu4=";
+      }
+      {
+        name = "copilot-chat";
+        publisher = "GitHub";
+        version = "0.9.2023101202";
+        sha256 = "sha256-DZnWNwmpNWHYMR3ycGyidyOPWSeJ0OMRFbjjnFj4oNo=";
+      }
+    ];
+    userSettings = {
+      workbench.colorTheme = "Ayu Mirage Bordered";
+      github.copilot = {
+        inlineSuggest.enable = false;
+        enable = {
+          "*" = true;
+          yaml = false;
+          plaintext = false;
+          markdown = false;
+        };
+      };
+    };
+  };
 }
