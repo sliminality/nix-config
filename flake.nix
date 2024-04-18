1d65b3eee3bba255d9b002acb76f60ab431d0324
Slim Lim <slim@makenotion.com>
2024-04-17 21:49:49 -0700

    Try using stable release 23.11

--- flake.nix
+++ flake.nix
@@ -2,14 +2,16 @@
   description = "nix-darwin and Home Manager configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     darwin = {
       # url = "github:lnl7/nix-darwin";
       url = "github:wegank/nix-darwin/mddoc-remove";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager";
+      # url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nur.url = "github:nix-community/NUR";
